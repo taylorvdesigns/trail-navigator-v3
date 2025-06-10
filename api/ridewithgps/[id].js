730585028149221744a230f891a7a32956f496cb
taylorvdesigns <taylorv@gmail.com>
2025-06-09 21:56:05 -0400

    Fix RideWithGPS API endpoint to use correct /routes/[id].json format

--- api/ridewithgps/[id].js
+++ api/ridewithgps/[id].js
@@ -1,3 +1,21 @@
+const axios = require('axios');
+
 module.exports = async (req, res) => {
-  res.status(200).json({ message: "Test function is working!", id: req.query.id });
+  const { id } = req.query;
+  try {
+    const apiUrl = `https://ridewithgps.com/routes/${id}.json`;
+    const params = {
+      version: 2,
+      apikey: process.env.RIDEWITHGPS_API_KEY,
+      auth_token: process.env.RIDEWITHGPS_AUTH_TOKEN
+    };
+    const response = await axios.get(apiUrl, { params });
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(error.response?.status || 500).json({
+      error: 'Failed to fetch route data',
+      details: error.message,
+      status: error.response?.status
+    });
+  }
 }; 
